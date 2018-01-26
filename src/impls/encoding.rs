8636a47a080f48c010fdfea5ed9d4354bf7067b9
Steven Fackler <sfackler@palantir.com>
2018-01-25 17:30:40 -0800

    Add Encoding

---
+++ src/impls/encoding.rs
@@ -0,0 +1,64 @@
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Inner {
+    Chunked,
+    Brotli,
+    Gzip,
+    Deflate,
+    Compress,
+    Identity,
+    Trailers,
+    Other(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Encoding(Inner);
+
+impl Encoding {
+    pub const CHUNKED: Encoding = Encoding(Inner::Chunked);
+
+    pub const BROTLI: Encoding = Encoding(Inner::Brotli);
+
+    pub const GZIP: Encoding = Encoding(Inner::Gzip);
+
+    pub const DEFLATE: Encoding = Encoding(Inner::Deflate);
+
+    pub const COMPRESS: Encoding = Encoding(Inner::Compress);
+
+    pub const IDENTITY: Encoding = Encoding(Inner::Identity);
+
+    pub const TRAILERS: Encoding = Encoding(Inner::Trailers);
+
+    pub fn new(encoding: &str) -> Encoding {
+        match encoding {
+            "chunked" => Encoding::CHUNKED,
+            "br" => Encoding::BROTLI,
+            "gzip" => Encoding::GZIP,
+            "deflate" => Encoding::DEFLATE,
+            "compress" => Encoding::COMPRESS,
+            "identity" => Encoding::IDENTITY,
+            "trailers" => Encoding::TRAILERS,
+            s => Encoding(Inner::Other(s.to_string())),
+        }
+    }
+
+    pub fn as_str(&self) -> &str {
+        match self.0 {
+            Inner::Chunked => "chunked",
+            Inner::Brotli => "br",
+            Inner::Gzip => "gzip",
+            Inner::Deflate => "deflate",
+            Inner::Compress => "compress",
+            Inner::Identity => "identity",
+            Inner::Trailers => "trailers",
+            Inner::Other(ref s) => s,
+        }
+    }
+}
+
+impl fmt::Display for Encoding {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.write_str(self.as_str())
+    }
+}
--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -1,3 +1,7 @@
+pub use impls::content_encoding::*;
+pub use impls::encoding::*;
 pub use impls::host::*;
 
+mod content_encoding;
+mod encoding;
 mod host;
